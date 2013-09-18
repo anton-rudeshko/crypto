e6bcca0022e618c67af6f8c7db56f78307286c00
Anton Rudeshko <rudeshko@yandex-team.ru>
2013-09-18 10:30:57 +0400

    More crypto utils!

--- CryptoUtils.js
+++
@@ -1,5 +0,0 @@
-var CryptoUtils = module.exports = {
-    fromHexString: function(input) {
-        return input.match(/.{2}/g).map(function(twoChars) { return parseInt(twoChars, 16); });
-    }
-};
